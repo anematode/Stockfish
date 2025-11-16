25766e7af422fdcd94a3433b462fda2384e649ee
Timothy Herchen <timothy.herchen@gmail.com>
2025-11-16 02:49:02 -0800

    experiment with sorting networks with cmov, bench 2559812

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -56,19 +56,73 @@ enum Stages {
     QCAPTURE
 };
 
-void i64_sort(ExtMove* begin, ExtMove* end) {
+template <int... Is>
+struct SortingNetwork {
+	static void sort(int64_t *begin) { }
+};
+
+template <>
+struct SortingNetwork <> {
+	static void sort(int64_t *begin) { }
+};
+
+typedef int64_t __attribute__((may_alias)) i64;
 	
-}
+template <int A, int B, int... Is>
+struct SortingNetwork <A, B, Is...> {
+	static_assert(sizeof...(Is) % 2 == 0);
+	static_assert(A != B);
+
+	static __attribute__((always_inline)) void sort_impl(i64 *begin) {
+		i64 a = begin[A], b = begin[B], c;
+		asm (
+			"cmpq %[b], %[a]\n"
+			"mov %[b], %[c]\n"
+			"cmovl %[a], %[b]\n"
+			"cmovl %[c], %[a]\n"
+		: [a]"+r"(a), [b]"+r"(b), [c]"=r"(c)
+		);
+		begin[A] = a;
+		begin[B] = b;
+
+		SortingNetwork<Is...>::sort(begin);
+	}
+
+	static void sort(i64 *begin) {
+		sort_impl(begin);
+	}
+};
+
+constexpr int NUM_HANDLERS = 15;
+static void (*handlers[NUM_HANDLERS])(i64*) = {
+	SortingNetwork<>::sort,
+	SortingNetwork<>::sort,
+	SortingNetwork<0, 1>::sort,
+	SortingNetwork<0, 2, 0, 1, 1, 2>::sort,
+	SortingNetwork<0, 2, 1, 3, 0, 1, 2, 3, 1, 2>::sort,
+	SortingNetwork<0, 3, 1, 4, 0, 2, 1, 3, 0, 1, 2, 4, 1, 2, 3, 4, 2, 3>::sort,
+	SortingNetwork<0, 5, 1, 3, 2, 4, 1, 2, 3, 4, 0, 3, 2, 5, 0, 1, 2, 3, 4, 5, 1, 2, 3, 4>::sort,
+	SortingNetwork<0, 6, 2, 3, 4, 5, 0, 2, 1, 4, 3, 6, 0, 1, 2, 5, 3, 4, 1, 2, 4, 6, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6>::sort,
+	SortingNetwork<0, 2, 1, 3, 4, 6, 5, 7, 0, 4, 1, 5, 2, 6, 3, 7, 0, 1, 2, 3, 4, 5, 6, 7, 2, 4, 3, 5, 1, 4, 3, 6, 1, 2, 3, 4, 5, 6>::sort,
+	SortingNetwork<0, 3, 1, 7, 2, 5, 4, 8, 0, 7, 2, 4, 3, 8, 5, 6, 0, 2, 1, 3, 4, 5, 7, 8, 1, 4, 3, 6, 5, 7, 0, 1, 2, 4, 3, 5, 6, 8, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6>::sort,
+	SortingNetwork<0,8,1,9,2,7,3,5,4,6,0,2,1,4,5,8,7,9,0,3,2,4,5,7,6,9,0,1,3,6,8,9,1,5,2,3,4,8,6,7,1,2,3,5,4,6,7,8,2,3,4,5,6,7,3,4,5,6>::sort,
+	SortingNetwork<0,9,1,6,2,4,3,7,5,8,0,1,3,5,4,10,6,9,7,8,1,3,2,5,4,7,8,10,0,4,1,2,3,7,5,9,6,8,0,1,2,6,4,5,7,8,9,10,2,4,3,6,5,7,8,9,1,2,3,4,5,6,7,8,2,3,4,5,6,7>::sort,
+	SortingNetwork<0,8,1,7,2,6,3,11,4,10,5,9,0,1,2,5,3,4,6,9,7,8,10,11,0,2,1,6,5,10,9,11,0,3,1,2,4,6,5,7,8,11,9,10,1,4,3,5,6,8,7,10,1,3,2,5,6,9,8,10,2,3,4,5,6,7,8,9,4,6,5,7,3,4,5,6,7,8>::sort,
+	SortingNetwork<0,12,1,10,2,9,3,7,5,11,6,8,1,6,2,3,4,11,7,9,8,10,0,4,1,2,3,6,7,8,9,10,11,12,4,6,5,9,8,11,10,12,0,5,3,8,4,7,6,11,9,10,0,1,2,5,6,9,7,8,10,11,1,3,2,4,5,6,9,10,1,2,3,4,5,7,6,8,2,3,4,5,6,7,8,9,3,4,5,6>::sort,
+	SortingNetwork<0,1,2,3,4,5,6,7,8,9,10,11,12,13,0,2,1,3,4,8,5,9,10,12,11,13,0,4,1,2,3,7,5,8,6,10,9,13,11,12,0,6,1,5,3,9,4,10,7,13,8,12,2,10,3,11,4,6,7,9,1,3,2,8,5,11,6,7,10,12,1,4,2,6,3,5,7,11,8,10,9,12,2,4,3,6,5,8,7,10,9,11,3,4,5,6,7,8,9,10,6,7>::sort
+};
 
 // Sort moves in descending order up to and including a given limit.
 // The order of moves smaller than the limit is left unspecified.
 void insertion_sort(ExtMove* begin, ExtMove* end) {
-	if (end - begin >= 8) {
-		i64_sort(begin, end);
-		//return;
+	if (end - begin < NUM_HANDLERS) {
+		handlers[end - begin](reinterpret_cast<i64*>(begin));
+		return;
 	}
 
-    for (ExtMove *sortedEnd = begin, *p = begin + 1; p < end; ++p) {
+	handlers[NUM_HANDLERS - 1](reinterpret_cast<i64*>(begin));
+
+    for (ExtMove *sortedEnd = begin + NUM_HANDLERS - 2, *p = sortedEnd + 1; p < end; ++p) {
 		ExtMove tmp = *p, *q;
 		*p          = *++sortedEnd;
 		for (q = sortedEnd; q != begin && *(q - 1) < tmp; --q)
