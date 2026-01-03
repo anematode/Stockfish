c55bbdb81cf85b23725b9666e9c770b7cb9d8f4f
anematode <timothy.herchen@gmail.com>
2026-01-02 21:54:26 -0800

    save

--- scripts/make_fat.py
+++ scripts/make_fat.py
@@ -11,25 +11,30 @@ SCRIPTS_DIR = os.path.dirname(os.path.abspath(__file__))
 SRC_DIR = os.path.join(os.path.dirname(SCRIPTS_DIR), "src")
 BUILDS_DIR = os.path.join(os.path.dirname(SCRIPTS_DIR), "builds")
 
+files = check_call(["git", "ls-files"], cwd=SRC_DIR).splitlines()
+print(files)
+
 class Arch:
 	def __init__(self, name: str):
 		self.name = name
 		# e.g. Stockfish_x86_64_avx2
 		self.namespace = "Stockfish_" + name.replace("-","_")
-		self.build_dir = os.path.join(os.path.dirname(BUILDS_DIR, self.name))
+		self.build_dir = os.path.join(BUILDS_DIR, self.name)
 
-	def set_up():
+	def set_up(self):
 		# delete the build dir if it already exists, then create a src directory
 		# and symlink all non-gitignored files, as well as NNUE files, to the main src
-		shutil.rmtree(self.build_dir)
+		try:
+			shutil.rmtree(self.build_dir)
+		except FileNotFoundError:
+			pass
 		os.makedirs(self.build_dir, exist_ok=True)
 
 		directory = Path(SRC_DIR)
 		for item in directory.iterdir():
-			if item.is_dir():
-				os.symlink
-			else:
-				item.unlink()
+			if item.is_dir() or include_file(item.name):
+				os.symlink(os.path.join(self.build_dir, item.name), item)
+			print(item, item.is_dir())
 		
 	name: str
 	namespace: str
@@ -43,22 +48,20 @@ def read_arches(s: str):
 	if VERBOSE:
 		joined = ",".join(matches)
 		print(f"Found {len(matches)} arches: {joined}");
-	return matches
+	return [*map(Arch, matches)]
 
 def run_make(path: str, args: list[str]):
 	if VERBOSE:
 		print("Running: make " + " ".join(args))
 	env = os.environ.copy()
-	check_call(["make", *args], env=env)
+	check_call(["make", *args], env=env, cwd=path)
 
 file_path = os.path.realpath(__file__)
 arches = read_arches(open(os.path.join(SRC_DIR, "Makefile"), "r").read())
 
-# First run make clean; make -j build in the root src directory so as to ensure
-# the requisite files are downloaded.
+# First run make net in the main src directory to ensure the nets are downloaded.
 
-run_make(SRC_DIR, ["clean"])
-run_make(SRC_DIR, ["-j", "build"])
+run_make(SRC_DIR, ["net"])
 
 for arch in arches:
-	setup_build_dir(
+	arch.set_up()
