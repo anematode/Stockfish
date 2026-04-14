75edee5263133b531b42399cacffbbc62def6a94
Tony <Tony>
2026-04-14 15:54:43 -0700

    add avx2 optimization

--- src/nnue/layers/sqr_clipped_relu.h
+++ src/nnue/layers/sqr_clipped_relu.h
@@ -68,12 +68,63 @@ class SqrClippedReLU {
     void propagate(const InputType* input, OutputType* output) const {
     // Simd branches assume alignas(64)
     static_assert(CacheLineSize >= 64 && CacheLineSize % 64 == 0, "CacheLineSize must be a multiple of 64 for SIMD optimizations");
+        static_assert(WeightScaleBitsLocal == 6 || WeightScaleBitsLocal == 7,
+                      "SqrClippedReLU SIMD paths only supports WeightScaleBitsLocal 6 or 7");
+
+#if defined(USE_AVX2)
+        constexpr IndexType NumChunks256 = InputDimensions / 32;
+        const __m256i Offsets = _mm256_set_epi32(7, 3, 6, 2, 5, 1, 4, 0);
+        const auto in256  = reinterpret_cast<const __m256i*>(input);
+        const auto out256 = reinterpret_cast<__m256i*>(output);
+
+        // 32-element chunks
+        for (IndexType i = 0; i < NumChunks256; ++i)
+        {
+            __m256i words0 = _mm256_packs_epi32(_mm256_load_si256(&in256[i * 4 + 0]), _mm256_load_si256(&in256[i * 4 + 1]));
+            __m256i words1 = _mm256_packs_epi32(_mm256_load_si256(&in256[i * 4 + 2]), _mm256_load_si256(&in256[i * 4 + 3]));
+
+            if constexpr (WeightScaleBitsLocal == 6) {
+                words0 = _mm256_srli_epi16(_mm256_mulhi_epi16(words0, words0), 3);
+                words1 = _mm256_srli_epi16(_mm256_mulhi_epi16(words1, words1), 3);
+            } else if constexpr (WeightScaleBitsLocal == 7) {
+                words0 = _mm256_srli_epi16(_mm256_mulhi_epi16(words0, words0), 5);
+                words1 = _mm256_srli_epi16(_mm256_mulhi_epi16(words1, words1), 5);
+            }
+
+            __m256i packed = _mm256_packs_epi16(words0, words1);
+            _mm256_store_si256(&out256[i], _mm256_permutevar8x32_epi32(packed, Offsets));
+        }
+
+        constexpr IndexType Start256 = NumChunks256 * 32;
+        constexpr IndexType NumChunks128 = (InputDimensions - Start256) / 16;
+
+        // 16-element remainder cascade
+        if constexpr (NumChunks128 > 0)
+        {
+            const auto in128  = reinterpret_cast<const __m128i*>(input + Start256);
+            const auto out128 = reinterpret_cast<__m128i*>(output + Start256);
+
+            for (IndexType i = 0; i < NumChunks128; ++i)
+            {
+                __m128i words0 = _mm_packs_epi32(_mm_load_si128(&in128[i * 4 + 0]), _mm_load_si128(&in128[i * 4 + 1]));
+                __m128i words1 = _mm_packs_epi32(_mm_load_si128(&in128[i * 4 + 2]), _mm_load_si128(&in128[i * 4 + 3]));
+
+                if constexpr (WeightScaleBitsLocal == 6) {
+                    words0 = _mm_srli_epi16(_mm_mulhi_epi16(words0, words0), 3);
+                    words1 = _mm_srli_epi16(_mm_mulhi_epi16(words1, words1), 3);
+                } else if constexpr (WeightScaleBitsLocal == 7) {
+                    words0 = _mm_srli_epi16(_mm_mulhi_epi16(words0, words0), 5);
+                    words1 = _mm_srli_epi16(_mm_mulhi_epi16(words1, words1), 5);
+                }
+
+                _mm_store_si128(&out128[i], _mm_packs_epi16(words0, words1));
+            }
+        }
+        constexpr IndexType Start = Start256 + (NumChunks128 * 16);
 
 #if defined(USE_SSE2)
         constexpr IndexType NumChunks = InputDimensions / 16;
 
-        static_assert(WeightScaleBitsLocal == 6 || WeightScaleBitsLocal == 7,
-                      "SqrClippedReLU SSE2 path only supports WeightScaleBitsLocal 6 or 7");
 
         const auto in  = reinterpret_cast<const __m128i*>(input);
         const auto out = reinterpret_cast<__m128i*>(output);
