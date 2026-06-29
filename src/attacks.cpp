3063c06270e4950494e38b0bf8939fe4d7dcba40
anematode <timothy.herchen@gmail.com>
2026-06-28 18:12:49 -0700

    constexpr all the things

--- src/attacks.cpp
+++ src/attacks.cpp
@@ -26,9 +26,40 @@ namespace Stockfish::Attacks {
 
 namespace {
 
-Bitboard LineBB[SQUARE_NB][SQUARE_NB];
-Bitboard BetweenBB[SQUARE_NB][SQUARE_NB];
-Bitboard RayPassBB[SQUARE_NB][SQUARE_NB];
+constexpr auto LineBB = []() {
+    std::array<std::array<Bitboard, SQUARE_NB>, SQUARE_NB> bb{};
+    for (Square s1 = SQ_A1; s1 <= SQ_H8; ++s1)
+        for (PieceType pt : {BISHOP, ROOK})
+            for (Square s2 = SQ_A1; s2 <= SQ_H8; ++s2)
+                if (PseudoAttacks[pt][s1] & s2)
+                    bb[s1][s2] = (sliding_attack(pt, s1, 0) & sliding_attack(pt, s2, 0)) | s1 | s2;
+    return bb;
+}();
+
+constexpr auto BetweenBB = []() {
+    std::array<std::array<Bitboard, SQUARE_NB>, SQUARE_NB> bb{};
+    for (Square s1 = SQ_A1; s1 <= SQ_H8; ++s1)
+        for (PieceType pt : {BISHOP, ROOK})
+            for (Square s2 = SQ_A1; s2 <= SQ_H8; ++s2)
+            {
+                if (PseudoAttacks[pt][s1] & s2)
+                    bb[s1][s2] =
+                      sliding_attack(pt, s1, square_bb(s2)) & sliding_attack(pt, s2, square_bb(s1));
+                bb[s1][s2] |= s2;
+            }
+    return bb;
+}();
+
+constexpr auto RayPassBB = []() {
+    std::array<std::array<Bitboard, SQUARE_NB>, SQUARE_NB> bb{};
+    for (Square s1 = SQ_A1; s1 <= SQ_H8; ++s1)
+        for (PieceType pt : {BISHOP, ROOK})
+            for (Square s2 = SQ_A1; s2 <= SQ_H8; ++s2)
+                if (PseudoAttacks[pt][s1] & s2)
+                    bb[s1][s2] =
+                      sliding_attack(pt, s1, 0) & (sliding_attack(pt, s2, square_bb(s1)) | s2);
+    return bb;
+}();
 
 #ifdef USE_DUAL_HYPERBOLA_QUINT
 alignas(64) DualMagic DualMagics[SQUARE_NB];
@@ -227,23 +258,6 @@ void init() {
     init_magics(ROOK, const_cast<MagicMask*>(RookTable.data()), Magics, true);
     init_magics(BISHOP, const_cast<MagicMask*>(BishopTable.data()), Magics, true);
 #endif
-
-    for (Square s1 = SQ_A1; s1 <= SQ_H8; ++s1)
-    {
-        for (PieceType pt : {BISHOP, ROOK})
-            for (Square s2 = SQ_A1; s2 <= SQ_H8; ++s2)
-            {
-                if (PseudoAttacks[pt][s1] & s2)
-                {
-                    LineBB[s1][s2] = (attacks_bb(pt, s1, 0) & attacks_bb(pt, s2, 0)) | s1 | s2;
-                    BetweenBB[s1][s2] =
-                      (attacks_bb(pt, s1, square_bb(s2)) & attacks_bb(pt, s2, square_bb(s1)));
-                    RayPassBB[s1][s2] =
-                      attacks_bb(pt, s1, 0) & (attacks_bb(pt, s2, square_bb(s1)) | s2);
-                }
-                BetweenBB[s1][s2] |= s2;
-            }
-    }
 }
 
 #ifdef USE_DUAL_HYPERBOLA_QUINT
